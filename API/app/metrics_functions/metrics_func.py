d4cae178ce7df983ae65ed50cb49bcf982bf63ae
Roman Ovsienko <roma-xom@mail.ru>
2025-07-03 09:54:08 +0600

    Дополнение к исправлению ошибки с длинной фреймов в функции метрик

--- API/app/metrics_functions/metrics_func.py
+++ API/app/metrics_functions/metrics_func.py
@@ -4,12 +4,14 @@ def calculate_metrics(real_data,predicted_data):
     length_real = len(real_data)
     length_predict = len(predicted_data)
     if length_predict > length_real:
-        predicted_data = predicted_data[:length_real]
+        predicted_data_sh = predicted_data.iloc[:length_real]
+        real_data_sh = real_data
     elif length_real > length_predict:
-        real_data = real_data[:length_predict]
+        real_data_sh = real_data.iloc[:length_predict]
+        predicted_data_sh = predicted_data
 
-    return {"R^2":r2_score(real_data,predicted_data),
-            "MAE":mean_absolute_error(real_data,predicted_data),
-            "MAPE":mean_absolute_percentage_error(real_data,predicted_data),
-            "MSE":mean_squared_error(real_data,predicted_data)
+    return {"R^2":r2_score(real_data_sh, predicted_data_sh),
+            "MAE":mean_absolute_error(real_data_sh, predicted_data_sh),
+            "MAPE":mean_absolute_percentage_error(real_data_sh, predicted_data_sh),
+            "MSE":mean_squared_error(real_data_sh, predicted_data_sh)
             }
