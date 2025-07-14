4e3ae6bddaeafd2a4a8525692e2f0bc888431df8
Roman Ovsienko <roma-xom@mail.ru>
2025-07-14 09:08:25 +0600

    Правка функций расчета предсказания и метрик

--- API/app/metrics_functions/metrics_func.py
+++ API/app/metrics_functions/metrics_func.py
@@ -10,8 +10,13 @@ def calculate_metrics(real_data,predicted_data):
     elif length_real > length_predict:
         real_data_sh = real_data.iloc[:length_predict]
         predicted_data_sh = predicted_data
+    else:
+        real_data_sh = real_data
+        predicted_data_sh = predicted_data
 
-    return {"R^2":r2_score(real_data_sh, predicted_data_sh),
+    #
+    return {
+            "R^2": r2_score(real_data_sh, predicted_data_sh),
             "MAE":mean_absolute_error(real_data_sh, predicted_data_sh),
             "MAPE":mean_absolute_percentage_error(real_data_sh, predicted_data_sh),
             "MSE":mean_squared_error(real_data_sh, predicted_data_sh)
